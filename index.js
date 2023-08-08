bdd75156a0447713ce6117fb1c329a27cd4297e4
Anmol-Prasad <anmol.gpc@gmail.com>
2023-08-08 23:51:43 +0530

    Updated changes

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
-import Connection from "./Connection.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-dotenv.config();
-
-const username = process.env.username;
-const password = process.env.password;
-Connection(username, password);
 
 app.post("/tobraille", routes);
 app.post("/tosign", routes);
